15b4819a4c644e2816d2e1c0cf17f16fae97f7ab
JacksonSmithStudent <jasm076@student.oaisd.org>
2024-10-14 13:02:28 +0000

    finsihed

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arithmetic
+{
+   public class Program
+   {
+        static void Main(string[] args)
+        {
+            int addition = 2 + 2;
+
+            int subtraction = 2 - 2;
+
+            int multiplication = 2 * 2;
+
+            int divison = 2 / 2;
+
+            double math = 5.0 / 2.0;
+
+            int modulus = 25 % 7;
+
+            Console.WriteLine(addition);
+
+            Console.WriteLine(subtraction);
+
+            Console.WriteLine(multiplication);
+
+            Console.WriteLine(divison);
+
+            Console.WriteLine(math);
+
+            Console.WriteLine(modulus);
+
+
+   }        
+}
+}
